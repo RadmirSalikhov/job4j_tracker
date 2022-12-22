dc9d82dd479d0355a12c9c2fb3a853d96b76926d
RadmirSalikhov <radmir.sf.02@gmail.com>
2022-12-22 18:26:55 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,12 +5,7 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-
     private LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return this.created;
-    }
 
     public Item() {
     }
@@ -22,6 +17,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return this.created;
     }
 
     public int getId() {
