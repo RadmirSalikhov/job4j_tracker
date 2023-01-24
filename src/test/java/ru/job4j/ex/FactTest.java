9ee5350b1741dc2a37fa2e38c10644218d4c0706
RadmirSalikhov <radmir.sf.02@gmail.com>
2023-01-24 11:08:41 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -16,4 +16,12 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
     }
+
+    @Test
+    public void whenExceptionNorm() {
+        int n = 5;
+        int expected = 120;
+        int result = new Fact().calc(n);
+        assertThat(result).isEqualTo(expected);
+    }
 }
