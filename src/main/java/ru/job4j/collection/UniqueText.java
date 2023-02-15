2f477eef8873b629c225f42fa53f6b67fc8b62f5
RadmirSalikhov <radmir.sf.02@gmail.com>
2023-02-15 19:24:06 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        /* for-each origin -> new HashSet. */
+        for (String orig : origin) {
+            check.add(orig);
+        }
+        /* for-each text -> hashSet.contains */
+        for (String texts : text) {
+            if (!check.contains(texts)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
